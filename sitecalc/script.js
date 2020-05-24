8398258b08bd976d20be673af2fac53747b03d47
Walison <walyteodoro@gmail.com>
2020-05-24 19:07:57 -0300

    correcoes basicas

--- sitecalc/script.js
+++ sitecalc/script.js
@@ -1,3 +1,16 @@
 function calcGeometria() {
-    
+    let dit = document.querySelector('.txtdt')
+    let dil = document.querySelector('.txtdl')
+    let c = document.querySelector('.txtcobrimento') 
+    let bw = document.querySelector('.txtbw')
+    let h = document.querySelector('.txth')
+
+    let c1 = Number(c.value) + (Number(dit.value)/10) + (Number(dil.value)/20)
+    let u = 2 * (Number(bw.value)+Number(h.value))
+    let a = Number(bw.value)*Number(h.value)
+    let verifc1 = 2*c1
+    let verifau = a/u
+
+    // os innerHTML são só pra testes
+    res.innerHTML = `C1 = ${c1}cm, U = ${u}cm e A = ${a}cm²\n${verifc1}&le; He &le;${verifau}`
 }
